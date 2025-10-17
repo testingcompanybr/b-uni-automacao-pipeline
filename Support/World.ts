41462ff9c16f94d19106cc775c28e4eda6a0fbdd
Daniel Nunes da Fonte <danielnunesdafonte9@gmail.com>
2025-10-17 14:27:18 -0300

    ops: teste

--- Support/World.ts
+++ Support/World.ts
@@ -44,7 +44,9 @@ export class World extends CucumberWorld {
       console.log('💻 Iniciando driver web');
       const chromeOptions = new chrome.Options();
       // chromeOptions.addArguments('--start-maximized');
-      chromeOptions.addArguments('--headless');
+      chromeOptions.addArguments('--headless'); // executa sem abrir janela
+      chromeOptions.addArguments('--no-sandbox');
+      chromeOptions.addArguments('--disable-dev-shm-usage');
 
       this.webDriver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
       this.driver = this.webDriver;
